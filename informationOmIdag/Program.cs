83c954c2b33ef4a8c52dea8fd4c1c2796d325103
Martin Sandung <Martin Sandung@SANDUNG>
2022-09-07 13:00:47 +0200

    Add project files.

---
+++ informationOmIdag/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace informationOmIdag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DagensInfo();
+        }
+
+        public static void DagensInfo()
+        {
+            var dagensDatum = DateTime.Now;
+            var nummer = dagensDatum.DayOfYear;
+            var veckodag = dagensDatum.DayOfWeek;
+            var sommartid = dagensDatum.IsDaylightSavingTime();
+            var skottår = DateTime.IsLeapYear(DateTime.Now.Year);
+            
+            
+            
+            
+            
+            Console.WriteLine($"Datum     : {dagensDatum.ToString("dd MMM yyyy")}");
+            Console.WriteLine($"Nummer    : {nummer}");
+            Console.WriteLine($"Veckodag  : {veckodag}");
+            Console.WriteLine($"Sommartid : {sommartid}");
+            Console.WriteLine($"Skottår   : {skottår}");
+        }
+    }
+}
